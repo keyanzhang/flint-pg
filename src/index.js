7c209a28c1acd78991aaebf4422712a89d08f18b
Keyan Zhang <kz5@indiana.edu>
2015-11-08 02:14:15 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,27 @@
+view Main {
+  <h1>h1</h1>
+  <foo>foo</foo>
+  <div>div</div>
+
+  const reset = {
+    margin: 0,
+    padding: 0,
+    border: 0,
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  }
+
+
+  $ = {
+    width: '100%',
+    minHeight: '100vh',
+    background: 'rgba(248, 245, 240, 1)',
+  }
+
+  $h1 = reset
+
+  $foo = {
+    float: 'left',
+  }
+}
